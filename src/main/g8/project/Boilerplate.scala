8398b47fbaa264114c5ff7a784e5896a383ea612
Eric Peters <eric@peters.org>
2020-08-12 01:02:11 -0700

    Change cross-compile specific directories to match default sbt scala-2.11 vs scala_2.11 and fix logic around suffixes -/+ (#89)

--- src/main/g8/project/Boilerplate.scala
+++ src/main/g8/project/Boilerplate.scala
@@ -68,21 +68,26 @@ object Boilerplate {
         (unmanagedSourceDirectories in sc).value.flatMap { dir =>
           Seq(
             scalaPartV.value match {
-              case Some((2, y)) if y == 11 => new File(dir.getPath + "_2.11")
-              case Some((2, y)) if y == 12 => new File(dir.getPath + "_2.12")
-              case Some((2, y)) if y >= 13 => new File(dir.getPath + "_2.13")
+              case Some((2, y)) if y == 11 => Seq(new File(dir.getPath + "-2.11"))
+              case Some((2, y)) if y == 12 => Seq(new File(dir.getPath + "-2.12"))
+              case Some((2, y)) if y == 13 => Seq(new File(dir.getPath + "-2.13"))
+              case _                       => Nil
             },
 
             scalaPartV.value match {
-              case Some((2, n)) if n >= 12 => new File(dir.getPath + "_2.12+")
-              case _                       => new File(dir.getPath + "_2.12-")
+              case Some((2, n)) if n > 12  => Seq(new File(dir.getPath + "-2.12+"))
+              case Some((2, n)) if n == 12 => Seq(new File(dir.getPath + "-2.12+"), new File(dir.getPath + "-2.12-"))
+              case Some((2, n)) if n < 12  => Seq(new File(dir.getPath + "-2.12-"))
+              case _                       => Nil
             },
 
             scalaPartV.value match {
-              case Some((2, n)) if n >= 13 => new File(dir.getPath + "_2.13+")
-              case _                       => new File(dir.getPath + "_2.13-")
+              case Some((2, n)) if n > 13  => Seq(new File(dir.getPath + "-2.13+"))
+              case Some((2, n)) if n == 13 => Seq(new File(dir.getPath + "-2.13+"), new File(dir.getPath + "-2.13-"))
+              case Some((2, n)) if n < 13  => Seq(new File(dir.getPath + "-2.13-"))
+              case _                       => Nil
             },
-          )
+          ).flatten
         }
       }
     }
